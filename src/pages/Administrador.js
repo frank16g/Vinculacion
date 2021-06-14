b6e17aee585d058eb086029057f492d53f2a506c
AleMera <66742960+AleMera@users.noreply.github.com>
2021-06-14 17:43:08 -0500

    adminPage modales

--- src/pages/Administrador.js
+++ src/pages/Administrador.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Contenido from '../components/admin/Contenido';
 
-class Administrador extends Component{
-    render(){
-        return(
+class Administrador extends Component {
+    render() {
+        return (
             <div>
                 <div class="card">
                     <button type="button" class="btn btn-primary">Agregar Integrante</button>
